a44ff32817e586affefa98e4ee8a2a1fd2532800
Atesz014 <atesz014@gmail.com>
2022-05-24 13:51:02 +0200

    add pic

--- admindash/src/App.js
+++ admindash/src/App.js
@@ -1,15 +1,46 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./app.css";
 import SideBar from "./components/sidebar/SideBar";
 import TopBar from "./components/topbar/TopBar";
-import "./app.css";
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
-    <div className="App">
-    <TopBar/>
-    <div className="container">
-      <SideBar/>    
-    </div>
-    </div>
+    <Router>
+      <TopBar />
+      <div className="container">
+        <SideBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
